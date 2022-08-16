1237b6423309f75bf4f6d33d0dafda5c25881da2
Alina Ulasevich <juliwu2517@gmail.com>
2022-08-16 20:26:14 +0200

    init: guessing game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+  constructor() { }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.max = max;
+    this.min = min;
+    this.value = 0;
+  }
 
-    }
+  guess() {
+    this.value = Math.ceil((this.max + this.min) / 2);
+    return this.value;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.value;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.value;
+  }
 }
 
 module.exports = GuessingGame;
